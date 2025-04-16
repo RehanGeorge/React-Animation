f34556ffaae57e92d02baa4940362717b3a03203
Rehan George <rehan.george@gmail.com>
2025-04-16 12:35:54 +0530

    Add a staggering motion

--- animation/src/components/Modal.jsx
+++ animation/src/components/Modal.jsx
@@ -9,10 +9,11 @@ export default function Modal({ title, children, onClose }) {
         variants={{
           hidden: { opacity: 0, y: 30 },
           visible: { opacity: 1, y: 0 },
+          remove: { opacity: 0, y: 30, transition: { duration: 0.2 } },
         }}
         initial="hidden"
         animate="visible"
-        exit="hidden"
+        exit="remove"
         open className="modal">
         <h2>{title}</h2>
         {children}
