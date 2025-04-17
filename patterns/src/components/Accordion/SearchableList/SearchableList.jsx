a75dbaf87fb7f8e90076fca9dceb219ef6413816
Rehan George <rehan.george@gmail.com>
2025-04-17 19:29:19 +0530

    Add debouncing

--- patterns/src/components/Accordion/SearchableList/SearchableList.jsx
+++ patterns/src/components/Accordion/SearchableList/SearchableList.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-export default function SearchableList({ items, children }) {
+export default function SearchableList({ items, itemKeyFn, children }) {
+    const lastChange = useRef();
     const [searchTerm, setSearchTerm] = useState('');
 
     const searchResults = items.filter(item => JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase()));
 
     function handleChange(event) {
-        setSearchTerm(event.target.value);
+        if (lastChange.current) {
+            clearTimeout(lastChange.current);
+        }
+        
+        lastChange.current = setTimeout(() => {
+            lastChange.current = null;
+            setSearchTerm(event.target.value);
+        }, 500);
     }
 
     return (
@@ -14,7 +22,7 @@ export default function SearchableList({ items, children }) {
             <input type="text" placeholder="Search..." className="search-input" onChange={handleChange}/>
             <ul className="list">
                 {searchResults.map((item, index) => (
-                    <li key={index} className="list-item">{children(item)}</li>
+                    <li key={itemKeyFn(item)} className="list-item">{children(item)}</li>
                 ))}
             </ul>
         </div>
