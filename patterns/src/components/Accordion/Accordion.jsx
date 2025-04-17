4ce29f060d46c2dc895ebec5ba8fe798eebfcf77
Rehan George <rehan.george@gmail.com>
2025-04-17 14:19:46 +0530

    Add context

--- patterns/src/components/Accordion/Accordion.jsx
+++ patterns/src/components/Accordion/Accordion.jsx
@@ -1,7 +1,43 @@
+import { createContext, useContext, useState } from "react"
+
+const AccordionContext = createContext();
+
+export function useAccordionContext() {
+    const ctx = useContext(AccordionContext);
+
+    if (!ctx) {
+        throw new Error("Accordion-related elements must be wrapped by <Accordion>.")
+    }
+
+    return ctx;
+}
+
 export default function Accordion({ children, className }) {
+    const [openItemId, setOpenItemId] = useState(null);
+
+    function toggleItem(id) {
+        setOpenItemId(prevId => prevId === id ? null : id);
+    }
+
+    function openItem(id) {
+        setOpenItemId(id);
+    }
+
+    function closeItem() {
+        setOpenItemId(null);
+    }
+
+    const contextValue = {
+        openItemId,
+        openItem,
+        closeItem
+    }
+
     return (
-        <ul className={className}>
-            {children}
-        </ul>
+        <AccordionContext.Provider value={contextValue}>
+            <ul className={className}>
+                {children}
+            </ul>
+        </AccordionContext.Provider>
     )
 }
