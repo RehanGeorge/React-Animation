2ded514c5074223418aa1b05e287fe9ca6620909
Rehan George <rehan.george@gmail.com>
2025-04-19 19:32:08 +0530

    Initial commit

--- Typescript/with-typescript.js
+++
@@ -1,5 +0,0 @@
-function add(a, b) {
-    return a + b;
-}
-var result = add(2, 10);
-console.log(result);
--- Typescript/with-typescript.ts
+++
@@ -1,7 +0,0 @@
-function add(a: number, b: number): number {
-    return a + b;
-}
-
-const result: number = add(2, 10);
-
-console.log(result);
