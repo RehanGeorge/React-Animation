92c1d7e8382c8f3c58daf776c2c9da8c41b99c9f
Rehan George <rehan.george@gmail.com>
2025-04-20 19:43:52 +0530

    Add modules css

--- Typescript/src/App.tsx
+++ Typescript/src/App.tsx
@@ -1,15 +1,24 @@
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
+import { useState } from 'react';
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn Typescript'),
-  ]
+  const [todos, setTodos] = useState<Todo[]>([new Todo('Learn React'), new Todo('Learn Typescript')]);
+
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    }
+    );
+  }
 
   return (
     <>
       <div>
+        <NewTodo onAddTodo={addTodoHandler}/>
         <Todos items={todos} />
       </div>
     </>
